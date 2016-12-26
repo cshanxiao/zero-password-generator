74410efbc7b4ecf8d0b3bfb923db6d660a072804
Near <jlliu@gizwits.com>
2016-12-27 00:02:59 +0800

    设置软件字体；增加二进制工具；

---
+++ generator/core/binary_util.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+u'''
+@summary:
+@author: cshanxiao
+@date: 2016年12月26日
+'''
+
+class Binutil(object):
+
+    def __init__(self):
+        pass
+    
+    def to_bytes(self, text):
+        ur'''
+        :summary: 从起始位置，每两个字母间插入'\x'
+        '''
+        text = text.replace(" ", "").replace("\t", "").strip()
+        if len(text) % 2 != 0:
+            return text
+        items = []
+        for i in range(len(text)):            
+            if i % 2 == 0 and i != 0:
+                continue
+            items.append(text[i-1: i+1])
+        return "\\x".join(items) 
+    
+
+if __name__ == "__main__":
+    util = Binutil()
+    print util.to_bytes("00000003  12  00  0090  01  0100000000000064000000000000")
+    
+    
+    
--- main.py
+++ main.py
@@ -5,11 +5,24 @@ u'''
 @date: 2016年2月18日
 '''
 import sys
+
 from PyQt4 import QtGui
+from PyQt4.QtGui import QFontDatabase, QFont
+
 from generator.control.main_dlg import Maindlg
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
+    
+    # 设置字体
+    loadedFontID = QFontDatabase.addApplicationFont(
+        "./resource/fonts/SourceCodePro-LightIt.ttf")
+    loadedFontFamilies = QFontDatabase.applicationFontFamilies(loadedFontID)
+    fontName = loadedFontFamilies[0]
+    font  = QFont(fontName, 12)
+    app.setFont(font)
+    
     mdlg = Maindlg()
     mdlg.show()
     sys.exit(app.exec_())

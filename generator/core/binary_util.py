25f45358e525f74d42f5845acd66408ee78ca904
Near <jlliu@gizwits.com>
2016-12-27 00:08:38 +0800

    Add parser;

--- generator/core/binary_util.py
+++ generator/core/binary_util.py
@@ -24,6 +24,13 @@ class Binutil(object):
             items.append(text[i-1: i+1])
         return "\\x".join(items) 
     
+    def _parse_head(self, data):
+        pass
+    
+    def decode_cmd(self, data):
+        "00000003  12  00  0090  01  0100000000000064000000000000"
+        
+        
 
 if __name__ == "__main__":
     util = Binutil()
